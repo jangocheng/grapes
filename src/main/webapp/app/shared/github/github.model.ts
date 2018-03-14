48d627778966570a69201118ae226a8668654664
l10178 <l10178@163.com>
2018-03-14 23:13:04 +0800

    add github service

---
+++ src/main/webapp/app/shared/github/github.model.ts
@@ -0,0 +1,8 @@
+export class GitHub {
+    constructor(
+        public name: string,
+        public download: number,
+        public star: number,
+        public contributor: number
+    ) { }
+}
