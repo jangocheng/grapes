78ca849921f75bab5a0e367be6615e3c58832873
l10178 <l10178@163.com>
2018-04-18 20:20:21 +0800

    add scss support

--- grape-skins/src/main.js
+++ grape-skins/src/main.js
@@ -1,10 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/styles/index.scss';
 import app from './app';
 import router from './router';
+
+
+Vue.use(VueAxios, axios);
 
 Vue.use(ElementUI);
 
@@ -12,8 +18,8 @@ Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: {app},
-  template: '<app/>',
+    el: '#app',
+    router,
+    components: {app},
+    template: '<app/>',
 });
