cc127656a5b14acc91bb8030fb9873cbc452bb79
l10178 <l10178@163.com>
2018-04-18 21:20:05 +0800

    update global scss

--- grape-skins/src/app.vue
+++ grape-skins/src/app.vue
@@ -17,15 +17,7 @@
   };
 </script>
 
-<style>
-  /*#app {*/
-    /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-    /*-webkit-font-smoothing: antialiased;*/
-    /*-moz-osx-font-smoothing: grayscale;*/
-    /*text-align: center;*/
-    /*color: #2c3e50;*/
-    /*margin-top: 60px;*/
-  /*}*/
+<style rel="stylesheet/scss" lang="scss" scoped>
 
   .el-header, .el-footer {
     background-color: #B3C0D1;
@@ -48,16 +40,8 @@
     line-height: 160px;
   }
 
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-
-  /*.el-container:nth-child(5) .el-aside,*/
-  /*.el-container:nth-child(6) .el-aside {*/
-    /*line-height: 260px;*/
+  /*body > .el-container {*/
+    /*!*margin-bottom: 40px;*!*/
   /*}*/
 
-  /*.el-container:nth-child(7) .el-aside {*/
-    /*line-height: 320px;*/
-  /*}*/
 </style>
