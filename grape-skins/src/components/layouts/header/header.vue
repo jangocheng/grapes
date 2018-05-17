148a709d9937beb93131adffcf17cb6b93d90f63
l10178 <l10178@163.com>
2018-05-17 17:53:47 +0800

    add signin

--- grape-skins/src/components/layouts/header/header.vue
+++ grape-skins/src/components/layouts/header/header.vue
@@ -1,47 +1,87 @@
 <template>
-    <b-navbar toggleable="md" type="dark" variant="info">
+    <div>
+        <b-navbar toggleable="md" type="dark" variant="info">
 
-        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+            <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-        <b-navbar-brand href="#">Grapes</b-navbar-brand>
+            <b-navbar-brand href="#">Grapes</b-navbar-brand>
 
-        <b-collapse is-nav id="nav_collapse">
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
+            <b-collapse is-nav id="nav_collapse">
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
 
-                <b-nav-item-dropdown right>
-                    <template slot="button-content">
-                        <i class="fa fa-github fa-2x"></i>
-                    </template>
-                    <b-dropdown-item href="https://github.com/l10178/grapes" target="_blank">grapes</b-dropdown-item>
-                    <b-dropdown-item href="https://github.com/l10178/bits-pieces" target="_blank">bits-pieces
-                    </b-dropdown-item>
-                    <b-dropdown-item href="https://github.com/l10178/angular-pretty-size"
-                                     target="_blank">angular-pretty-size
-                    </b-dropdown-item>
-                </b-nav-item-dropdown>
+                    <b-nav-item-dropdown right>
+                        <template slot="button-content">
+                            <i class="fa fa-github fa-2x"></i>
+                        </template>
+                        <b-dropdown-item href="https://github.com/l10178/grapes" target="_blank">grapes
+                        </b-dropdown-item>
+                        <b-dropdown-item href="https://github.com/l10178/bits-pieces" target="_blank">bits-pieces
+                        </b-dropdown-item>
+                        <b-dropdown-item href="https://github.com/l10178/angular-pretty-size"
+                                         target="_blank">angular-pretty-size
+                        </b-dropdown-item>
+                    </b-nav-item-dropdown>
 
-                <b-nav-item-dropdown text="Lang" right>
-                    <b-dropdown-item href="#">English</b-dropdown-item>
-                    <b-dropdown-item href="#">Chinese</b-dropdown-item>
-                </b-nav-item-dropdown>
+                    <b-nav-item-dropdown text="Lang" right>
+                        <b-dropdown-item href="#">English</b-dropdown-item>
+                        <b-dropdown-item href="#">Chinese</b-dropdown-item>
+                    </b-nav-item-dropdown>
 
-                <b-nav-item-dropdown right>
-                    <template slot="button-content">
-                        <em>User</em>
-                    </template>
-                    <b-dropdown-item href="#">Profile</b-dropdown-item>
-                    <b-dropdown-item href="#">Signout</b-dropdown-item>
-                </b-nav-item-dropdown>
-            </b-navbar-nav>
+                    <b-nav-item-dropdown right>
+                        <template slot="button-content">
+                            <em>User</em>
+                        </template>
+                        <b-dropdown-item href="#">Profile</b-dropdown-item>
+                        <b-dropdown-item href="#">Register</b-dropdown-item>
 
-        </b-collapse>
-    </b-navbar>
+                        <b-dropdown-item href="#" @click="signinFormVisible = true">
+                            <el-button type="text">Sign In</el-button>
+                        </b-dropdown-item>
+                        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+
+            </b-collapse>
+        </b-navbar>
+
+
+        <el-dialog title="Sign In" :visible.sync="signinFormVisible">
+            <el-form :model="user">
+                <el-form-item label="Username" :label-width="formLabelWidth">
+                    <el-input type="text" v-model="user.username" auto-complete="off" required></el-input>
+                </el-form-item>
+                <el-form-item label="Password" :label-width="formLabelWidth">
+                    <el-input type="password" v-model="user.password" auto-complete="off" required></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="signinFormVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="login()">Submit</el-button>
+            </div>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'GrpHeader',
+        data() {
+            return {
+                signinFormVisible: false,
+                user: {
+                    username: '',
+                    password: '',
+                },
+                formLabelWidth: '120px',
+            };
+        },
+        methods: {
+            login: function() {
+                this.signinFormVisible = false;
+                // this.axios.post('/test', this.user);
+            },
+        },
     };
 </script>
 
