e3eb29703424d094c39db7f3d3f387449cec80c6
l10178 <l10178@163.com>
2018-05-22 21:47:30 +0800

    remove ?

--- grape-skins/src/components/home/home.vue
+++ grape-skins/src/components/home/home.vue
@@ -64,7 +64,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12 text-center">
-                        <h2 class="heading">What is Grapes?</h2>
+                        <h2 class="heading">What is Grapes</h2>
                         <hr class="star-primary">
                     </div>
                 </div>
